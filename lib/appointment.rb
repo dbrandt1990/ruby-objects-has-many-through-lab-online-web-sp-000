7f7e57e9e6f32d730ab8bcaf005ef1751d4f1df8
Danny Brandt <dvbrandt90@gmail.com>
2021-01-11 16:01:10 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,2 +1,12 @@
 class Appointment
+  attr_reader :patient, :doctor
+  
+  @@all = []
+  
+  def initialize(patient, doctor)
+    @patient = patient
+    @doctor = doctor
+    @@all << self
+  end
+  
 end 
