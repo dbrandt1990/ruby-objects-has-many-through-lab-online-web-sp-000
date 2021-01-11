6e3bbd723a120e436a3e468c1c41656196b788c5
Danny Brandt <dvbrandt90@gmail.com>
2021-01-11 16:13:48 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -16,4 +16,11 @@ class Patient
     Appointment.all.select {|app| app.patient.name == @name}
   end
   
+  def doctors 
+    Appointment.all.map do |app|
+      if app.patient.name == @name 
+        app.doctor 
+      end
+  end
+  
 end
